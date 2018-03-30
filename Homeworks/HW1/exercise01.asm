a68e0790116f857abdc7082787edd58e6c7c32f4
Davide <davide@fortyzone.it>
2018-03-30 20:53:36 +0200

    HW1: a little improvement and some comments
    I think it's ok to submit.

--- Homeworks/HW1/exercise01.asm
+++ Homeworks/HW1/exercise01.asm
@@ -18,22 +18,25 @@
 .text
 
 main:
-	jal nextInt
+	jal nextInt					# get k
 	move $k, $v0
+	
+	jal sequenceLoop			# core of the program
 
-	jal sequenceLoop
 	endCore:
-	jal printStats
+	jal printStats				# print minimums and maximums
 
-	# exit
+	exit:
 	li $v0, 10
 	syscall
 	
+# get an integer via syscall 5 #
 nextInt:
 	li $v0, 5
 	syscall
 	jr $ra
-	
+
+# print the integer in $a0, followed by "\n"	
 printInt:
 	li $v0, 1
 	syscall
@@ -44,6 +47,7 @@ printInt:
 				
 	jr $ra
 	
+# print the statistics of S and Y
 printStats:
 	move $a0, $minS
 	jal printInt
@@ -54,23 +58,25 @@ printStats:
 	move $a0, $maxY
 	jal printInt
 	
-	jr $ra
+	j exit
 	
+# loop that gets an integer at a time, computes statistics and updates the sliding window
 sequenceLoop:
-	jal nextInt
-	beqz $v0, endSequenceLoop
-	sll $i, $i, 2
-	sw $v0, window($i)
-	srl $i, $i, 2
+	jal nextInt							# get the next integer
+	beqz $v0, endSequenceLoop			# break loop if 0 is given
+	sll $i, $i, 2						# left-shift the index to use is as offset in the array
+	sw $v0, window($i)					# store the integer in the array
+	srl $i, $i, 2						# right-shift back the index to use it in the loop
 
 	# -- COMPUTE STATS FOR S -- #
-	bnez $isFirstOfSeq, doStatsS
-		move $minS, $v0					# load current integer
-		move $maxS, $v0					# load current integer
+	bnez $isFirstOfSeq, doStatsS		# if this is not the first number of the sequence, compute statistics
+		move $minS, $v0					# else initialize the minimum of S
+		move $maxS, $v0					# and the maximum of S
+		li $isFirstOfSeq, 1				# now the next number will not be the first anymore --> set it to false (1)
 		j afterStatsS
 	doStatsS:
-		blt $v0, $minS, setMinS
-		bgt $v0, $maxS, setMaxS
+		blt $v0, $minS, setMinS			# if this is the new minimum, update it
+		bgt $v0, $maxS, setMaxS			# else if this is the new maximum, update it
 		j afterStatsS
 		setMinS:
 			move $minS, $v0
@@ -79,24 +85,23 @@ sequenceLoop:
 			move $maxS, $v0
 			
 	afterStatsS:
-	li $isFirstOfSeq, 1					# set is as false
 	
 	# -- UPDATE SUM -- #
-	add $sum, $sum, $v0				# sum += currentInt
+	add $sum, $sum, $v0					# increment the sum with the current integer
 	
 	# -- PRINT Y ITEM OR NOT -- #
-	addi $t6, $k, -1
-	bne $i, $t6, afterWindowUpdate
+	addi $t6, $k, -1					# $t6 = k-1	
+	bne $i, $t6, afterWindowUpdate		# if we haven't already got k numbers, skip the math; else...
 		move $a0, $sum
-		jal printInt
-		bnez $isFirstStat, doStatsY
-			move $minY, $sum
-			move $maxY, $sum
-			li $isFirstStat, 1
+		jal printInt					# print the sum (uses $a0)
+		bnez $isFirstStat, doStatsY		# if this isn't going to be the first statistic, do calculate it
+			move $minY, $sum			# else initialize the minimum of Y
+			move $maxY, $sum			# and its maximum
+			li $isFirstStat, 1			# now the next is not going to be the first statistic anymore --> set it to false (1)
 			j afterStatsY
 		doStatsY:
-			blt $sum, $minY, setMinY
-			bgt $sum, $maxY, setMaxY
+			blt $sum, $minY, setMinY	# if this is the new minimum, update it
+			bgt $sum, $maxY, setMaxY	# else if this is the new maximum, update it
 			j afterStatsY
 			setMinY:
 				move $minY, $sum
@@ -105,28 +110,29 @@ sequenceLoop:
 				move $maxY, $sum
 
 		afterStatsY:
-		lw $t7, window($0)				# load first of window
-		sub $sum, $sum, $t7
-		jal shiftWindow
-		addi $i, $i, -1
+		lw $t7, window($0)				# load first number of window
+		sub $sum, $sum, $t7				# subtract it from the current sum
+		jal shiftWindow					# move all the items in the window, 1 position to left
+		addi $i, $i, -1					# now we have room for another integer --> decrement the counter
 		
 	afterWindowUpdate:
-	addi $i, $i, 1
+	addi $i, $i, 1						# increment counter
 	j sequenceLoop
 	
 endSequenceLoop:
 	j endCore
-	
+
+# shift 1 place to left, all the items in the window
 shiftWindow:
-	move $t6, $0							# counter
-	addi $s3, $k, -1
+	move $t6, $0							# counter i
+	addi $s3, $k, -1						# $s3 = k-1
 	loop:
-		bge $t6, $s3, endLoop				# i < len(window)-1?
+		bge $t6, $s3, endLoop				# if i>k-1, end this loop; else...
 		addi $t6, $t6, 1					# i++ (we need the next item)
 		sll $t6, $t6, 2						# i*=4
 		lw $t7, window($t6)					# load item from window
 		
-		beqz $t7, endLoop					# window[i+1] == 0?
+		beqz $t7, endLoop					# if window[i+1] == 0, end this loop (becaouse all the rest is 0); else...
 			srl $t6, $t6, 2					# i /= 4
 			addi $t6, $t6, -1				# i--
 			sll $t6, $t6, 2					# i *= 4
