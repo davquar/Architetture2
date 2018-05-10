c8b7656ee2d8f659d2254ad2587590f8b81b4cc2
Davide <davide@fortyzone.it>
2018-05-10 15:22:39 +0200

    nextStep done but not working

--- Homeworks/HW3/exercise03.asm
+++ Homeworks/HW3/exercise03.asm
@@ -5,6 +5,7 @@
 	vector: .word 15,-1,-2,-3,3,-1,7,-3,-666,-666,5,8,-666,-666,6,9
 	openBracket: .asciiz "("
 	closedBracket: .asciiz ")"
+	newLine: .asciiz "\n"
 	opChars: .byte '*','+','-','^'
 	mulChar: .asciiz "*"
 	addChar: .asciiz "+"
@@ -23,6 +24,12 @@
 .eqv $SUB, -3
 .eqv $POW, -4
 .eqv $EMPTY, -666
+
+.eqv $isLeafLeft $t3
+.eqv $isLeafRight $t4
+.eqv $opNode $t5
+.eqv $leftValue $t6
+.eqv $rightValue $t7
 
 .macro takeInt (%regDst)
 	li $v0, 5
@@ -106,6 +113,13 @@ main:
 #	endAskLoop:
 	addi $index, $0, 1
 	jal printExpression
+	jal nextStep
+	
+	la $a0, newLine
+	li $v0, 4
+	syscall
+	
+	jal printExpression
 	
 	li $v0, 10
 	syscall
@@ -129,7 +143,7 @@ printExpression:
 		readVector ($index, $a0)		# vector(4*$a1) --> $a0
 		li $v0, 1
 		syscall
-		j return
+		j returnPrint
 	
 	notLeaf:
 		mul	$left, $index, 2			# left index
@@ -145,7 +159,7 @@ printExpression:
 		addi $index, $index, 1
 		jal printExpression
 		
-	return:
+	returnPrint:
 		la $a0, closedBracket
 		li $v0, 4
 		syscall
@@ -154,3 +168,73 @@ printExpression:
 		lw $index, 4($sp)
 		addi $sp, $sp, 8
 		jr $ra
+
+# ------------------------------- #
+
+# a1: current index in vector
+nextStep:
+	addi $sp, $sp, -8
+	sw $ra, 0($sp)
+	sw $index, 4($sp)
+	
+	mul $left, $index, 2
+	addi $right, $left, 1
+	
+	bgt $left, $n, skipOperation
+	bgt $right, $n, skipOperation
+	
+	isLeaf ($left)
+	move $isLeafLeft, $v0
+	isLeaf ($right)
+	move $isLeafRight, $v0
+	
+	bne $isLeafLeft, 1, skipOperation
+	bne $isLeafRight, 1, skipOperation
+	
+	# do operation
+	readVector ($index, $opNode)
+	beq $opNode, $MUL, isMul
+	beq $opNode, $ADD, isAdd
+	beq $opNode, $SUB, isSub
+	beq $opNode, $POW, isPow
+
+	isAdd:
+		add $opNode, $leftValue, $rightValue
+		j writeResult
+	isMul:
+		mul $opNode, $leftValue, $rightValue
+		j writeResult
+	isSub:
+		sub $opNode, $leftValue, $rightValue
+		j writeResult
+	isPow:
+		# write pow
+		
+	writeResult:
+		sll $v0, $index, 2
+		sw $opNode, vector($v0)
+		sll $v0, $left, 2
+		li $a0, $EMPTY
+		sw $a0, vector($left)
+		sll $v0, $right, 2
+		sw $a0,  vector($right)
+		
+	j returnDo
+	# end operation
+	
+	skipOperation:
+	
+	bgt $left, $n, tryRight
+	move $index, $left
+	jal nextStep
+	
+	tryRight:
+		bgt $right, $n, returnDo
+		move $index, $right
+		jal nextStep
+	
+	returnDo:
+	lw $ra, 0($sp)
+	lw $index, 4($sp)
+	addi $sp, $sp, 8
+	jr $ra
